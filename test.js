2789e82d3c6f39122a9ce00bddd6fbec273146df
Nathan Rajlich <nathan@tootallnate.net>
2012-12-14 16:29:39 -0800

    lots of progress on the Track class

--- test.js
+++ test.js
@@ -3,25 +3,34 @@ var login = require('./login');
 var Despotify = require('./');
 var Speaker = require('speaker');
 
-var spotify = new Despotify(true);
+var spotify = new Despotify({ highBitrate: true });
 spotify.login(login.username, login.password, function (err) {
   if (err) throw err;
 
   // Champagne supernova
   var uri = process.argv[2] || 'spotify:track:4Jgp57InfWE4MxJLfheNVz';
-  var readable = spotify.play(uri);
-  readable.on('format', function (format) {
-    console.error('"format" event:', format);
-  });
-  readable.pipe(new Speaker());
+  spotify.play(uri);
 
   spotify.on('track', function (track) {
-    console.error(track.file_id);
-    console.error(track.file_bitrate);
-    console.error(track.key);
-    console.error(track.allowed);
-    console.error(track.forbidden);
-    console.error(track.length);
-    console.error(track.year);
+    console.error('"track" event');
+    console.error('title: %j', track.title);
+    console.error('album: %j', track.album);
+    console.error('bitrate: %d kbit/s', track.bitrate / 1000);
+    console.error('track id: %j', track.trackId);
+    console.error('cover id: %j', track.coverId);
+    console.error('length: %j', track.length);
+
+    // the spotify "track" is also a Readable stream instance
+    track.on('format', function (format) {
+      console.error('track "format" event:', format);
+      track.pipe(new Speaker(format));
+    });
+    track.on('end', function () {
+      console.error('track "end" event');
+    });
+    track.on('error', function (err) {
+      console.error('track "error" event:', err);
+    });
+
   });
 });
